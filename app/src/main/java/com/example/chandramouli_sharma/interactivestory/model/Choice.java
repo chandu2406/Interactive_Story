248b6b65232567c5572b0b79ea6597b417ae1b9b
chandu2406 <sharma.cms007@gmail.com>
2015-03-29 17:35:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chandramouli_sharma/interactivestory/model/Choice.java
@@ -0,0 +1,33 @@
+package com.example.chandramouli_sharma.interactivestory.model;
+
+/**
+ * Created by chandramouli-sharma on 23-03-2015.
+ */
+public class Choice {
+
+    private  String mText;
+    private int mNextPage;
+
+
+        public Choice(String text, int nextPage){
+
+            mText = text;
+            mNextPage = nextPage;
+
+        }
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
